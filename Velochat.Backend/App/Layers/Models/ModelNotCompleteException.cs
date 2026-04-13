08f2e5b7e8b6c6237453d8a7cffe676b56ed9e40
kw-00 <christopher.p.wojcik@gmail.com>
2026-04-13 18:25:34 +0200

    Improve error message for Model errors

--- Velochat.Backend/App/Layers/Models/ModelNotCompleteException.cs
+++ Velochat.Backend/App/Layers/Models/ModelNotCompleteException.cs
@@ -1,4 +1,7 @@
 namespace Velochat.Backend.App.Layers.Models;
 
 public class ModelNotCompleteException() 
-    : Exception("Model is not complete. Check whether all required fields are filled.");
+    : Exception(
+        "Model cannot be conveted to its complete form"
+        + " due to null or invalid values in certain fields."
+    );
--- Velochat.Backend/App/Layers/Models/ModelNotInsertableException.cs
+++ Velochat.Backend/App/Layers/Models/ModelNotInsertableException.cs
@@ -1,4 +1,7 @@
 namespace Velochat.Backend.App.Layers.Models;
 
 public class ModelNotInsertableException() 
-    : Exception("Model is not insertable. Check whether it does not have non-null values in fields such as ID.");
+    : Exception(
+        "Model is not ready for insertion. Make sure all required fields are filled,"
+        + " and fields like auto-generated primary keys are absent in the model."
+    );
