4bf7b99e8173d6ff7a9015bc83a40040d77e6291
kw-00 <christopher.p.wojcik@gmail.com>
2026-04-21 13:37:40 +0200

    Change data model to accomodate friendship

--- Velochat.Database/initdb/init.sql
+++ Velochat.Database/initdb/init.sql
@@ -7,16 +7,22 @@ CREATE TABLE identities (
     password_hash TEXT NOT NULL
 );
 
+CREATE TABLE friend_requests (
+    sender_id INT NOT NULL,
+    recipient_id INT NOT NULL,
+    CONSTRAINT pk_invitations PRIMARY KEY (sender_id, recipient_id)
+);
+
+CREATE TABLE friendships (
+    subject_1_id INT NOT NULL,
+    subject_2_id INT NOT NULL,
+    CONSTRAINT pk_friendships PRIMARY KEY (subject_1_id, subject_2_id)
+);
+
 CREATE TABLE room_presences (
     room_id INT NOT NULL,
     member_id INT NOT NULL,
     CONSTRAINT pk_room_presences PRIMARY KEY (room_id, member_id)
-);
-
-CREATE TABLE invitations (
-    room_id INT NOT NULL,
-    invitee_id INT NOT NULL,
-    CONSTRAINT pk_invitations PRIMARY KEY (room_id, invitee_id)
 );
 
 CREATE TABLE rooms (
@@ -38,6 +44,32 @@ CREATE TABLE refresh_token_states (
     status TEXT NOT NULL
 );
 
+/* Friendship */
+ALTER TABLE friend_requests ADD CONSTRAINT fk_friend_requests_sender_id
+    FOREIGN KEY (sender_id) REFERENCES identities (id)
+;
+
+ALTER TABLE friend_requests ADD CONSTRAINT fk_friend_requests_recipient_id
+    FOREIGN KEY (recipient_id) REFERENCES identities (id)
+;
+
+CREATE UNIQUE INDEX no_two_way_friend_requests ON friend_requests (
+    LEAST(sender_id, recipient_id), GREATEST(sender_id, recipient_id)
+);
+
+ALTER TABLE friendships ADD CONSTRAINT fk_friendships_subject_1_id
+    FOREIGN KEY (subject_1_id) REFERENCES identities (id)
+;
+
+ALTER TABLE friendships ADD CONSTRAINT fk_friendships_subject_2_id
+    FOREIGN KEY (subject_2_id) REFERENCES identities (id)
+;
+
+ALTER TABLE friendships ADD CONSTRAINT subject_order_check
+    CHECK (subject_1_id < subject_2_id)
+;
+
+/* Rooms */
 ALTER TABLE room_presences ADD CONSTRAINT fk_room_presences_room_id
     FOREIGN KEY (room_id) REFERENCES rooms (id)
 ;
@@ -46,20 +78,15 @@ ALTER TABLE room_presences ADD CONSTRAINT fk_room_presences_member_id
     FOREIGN KEY (member_id) REFERENCES identities (id)
 ;
 
-ALTER TABLE invitations ADD CONSTRAINT fk_invitations_room_id
-    FOREIGN KEY (room_id) REFERENCES rooms (id)
-;
-
-ALTER TABLE invitations ADD CONSTRAINT fk_invitations_identity_id
-    FOREIGN KEY (invitee_id) REFERENCES identities (id)
-;
-
 ALTER TABLE rooms ADD CONSTRAINT fk_rooms_owner_id
     FOREIGN KEY (owner_id) REFERENCES identities (id)
 ;
 
-ALTER TABLE rooms ADD CONSTRAINT unique_owner_and_name UNIQUE (owner_id, name);
+ALTER TABLE rooms ADD CONSTRAINT unique_owner_and_name 
+    UNIQUE (owner_id, name)
+;
 
+/* Chat */
 ALTER TABLE chat_messages ADD CONSTRAINT fk_chat_messages_room_id
     FOREIGN KEY (room_id) REFERENCES rooms (id)
 ;
@@ -68,6 +95,7 @@ ALTER TABLE chat_messages ADD CONSTRAINT fk_chat_messages_author_id
     FOREIGN KEY (author_id) REFERENCES identities (id)
 ;
 
+/* Refresh tokens */
 ALTER TABLE refresh_token_states ADD CONSTRAINT fk_refresh_token_states_identity_id
     FOREIGN KEY (identity_id) REFERENCES identities (id)
 ;
