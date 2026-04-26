60dbad8e42a788b0aa692cf4a16c2959beed69f9
kw-00 <christopher.p.wojcik@gmail.com>
2026-04-26 21:37:28 +0200

    Update server interface

--- Velochat.Frontend/src/app/infrastructure/server-interface.ts
+++ Velochat.Frontend/src/app/infrastructure/server-interface.ts
@@ -5,6 +5,9 @@ import { RoomsHubClient } from "./hub/rooms";
 import { ServerEvents } from "./hub/server-events";
 import { IdentityClient } from "./rest/identity-client";
 import { RealtimeConnection } from "./hub/realtime-connection";
+import { FriendshipManager } from "./friendship-manager";
+import { MessageManager } from "./message-manager";
+import { RoomManager } from "./room-manager";
 
 
 
@@ -21,11 +24,27 @@ export class ServerInterface {
             .build()
     );
 
-    readonly serverEvents = new ServerEvents(this._connection);
-    readonly messaging = new MessagingHubClient(this._connection);
-    readonly rooms = new RoomsHubClient(this._connection);
-    readonly friendship = new FriendshipHubClient(this._connection);
-    readonly identity = new IdentityClient();
+    private _serverEvents = new ServerEvents(this._connection);
+    private _friendshipHubClient = new FriendshipHubClient(this._connection);
+    private _roomsHubClient = new RoomsHubClient(this._connection);
+    private _messagingHubClient = new MessagingHubClient(this._connection);
+
+    readonly auth = new IdentityClient();
+    readonly friendship = new FriendshipManager(
+        this._connection, 
+        this._serverEvents, 
+        this._friendshipHubClient
+    );
+    readonly rooms = new RoomManager(
+        this._connection, 
+        this._serverEvents,
+        this._roomsHubClient
+    );
+    readonly messaging = new MessageManager(
+        this._connection, 
+        this._serverEvents,
+        this._messagingHubClient
+    );
 }
 
 
